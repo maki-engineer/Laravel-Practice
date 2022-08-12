e9ad22db824c7de6aebc5da48548d423d8d314c6
maki-engineer <ryohei-81@outlook.jp>
2022-08-12 11:28:45 +0900

    update: サービスプロバイダーの実現

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,7 +9,8 @@
 
 @section('content')
    <p>ここが本文のコンテンツです。</p>
-   <p>必要なだけ記述できます。</p>
+   <p>Controller value<br>"message" = {{$message}}</p>
+   <p>ViewComposer value<br>"view_message" {{$view_message}}</p>
 @endsection
 
 @section('footer')
