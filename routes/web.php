84407918d24e912286a614af9b2eed09afcb97a6
maki-engineer <ryohei-81@outlook.jp>
2022-08-15 18:20:32 +0900

    update: ルーティング先を追加

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get("/hello/add", "App\Http\Controllers\HelloController@add");
 Route::post("/hello/add", "App\Http\Controllers\HelloController@create");
 Route::get("/hello/edit", "App\Http\Controllers\HelloController@edit");
 Route::post("/hello/edit", "App\Http\Controllers\HelloController@update");
+Route::get("/hello/del", "App\Http\Controllers\HelloController@del");
+Route::post("/hello/del", "App\Http\Controllers\HelloController@remove");
