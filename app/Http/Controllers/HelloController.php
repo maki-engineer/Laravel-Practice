6a47f0e783e64cc48f66dc5d8cc21a56670a4a79
maki-engineer <ryohei-81@outlook.jp>
2022-08-16 14:22:46 +0900

    update: edit関数をクエリビルダ記法に変更

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -52,10 +52,10 @@ class HelloController extends Controller
 
   public function edit(Request $request)
   {
-    $param = ["id" => $request->id];
-    $item  = DB::select("select * from people where id = :id", $param);
+    $id = $request->id;
+    $item  = DB::table("people")->where("id", $id)->first();
 
-    return view("hello.edit", ["form" => $item[0]]);
+    return view("hello.edit", ["form" => $item]);
   }
 
   public function update(Request $request)
@@ -67,7 +67,7 @@ class HelloController extends Controller
       "age"  => $request->age,
     ];
 
-    DB::update("update people set name = :name, mail = :mail, age = :age where id = :id", $param);
+    DB::table("people")->where("id", $request->id)->update($param);
 
     return redirect("/hello");
   }
