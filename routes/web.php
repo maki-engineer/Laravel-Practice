fe5176b774164fbc385d127833d250ba8dbdcef1
maki-engineer <ryohei-81@outlook.jp>
2022-08-19 08:22:27 +0900

    update: create機能の実装

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::get("/hello/show", "App\Http\Controllers\HelloController@show");
 Route::get("/person", "App\Http\Controllers\PersonController@index");
 Route::get("/person/find", "App\Http\Controllers\PersonController@find");
 Route::post("/person/find", "App\Http\Controllers\PersonController@search");
+Route::get("/person/add", "App\Http\Controllers\PersonController@add");
+Route::post("/person/add", "App\Http\Controllers\PersonController@create");
