31ace09e65f2b4b93482fb170643c51f7d12a7c5
maki-engineer <ryohei-81@outlook.jp>
2022-08-19 08:41:48 +0900

    update: edit機能の実装

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -37,10 +37,32 @@ class PersonController extends Controller
     public function create(Request $request)
     {
         $this->validate($request, Person::$rules);
+
         $person = new Person;
         $form   = $request->all();
 
-        unset($form["_token"]);
+        unset($form["_token"]);  // _tokenの項目を削除
+
+        $person->fill($form)->save();
+
+        return redirect("/person");
+    }
+
+    public function edit(Request $request)
+    {
+        $person = Person::find($request->id);
+
+        return view("person.edit", ["form" => $person]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+
+        $person = Person::find($request->id);
+        $form   = $request->all();
+
+        unset($form["_token"]);  // _tokenの項目を削除
 
         $person->fill($form)->save();
 
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::get("/person/find", "App\Http\Controllers\PersonController@find");
 Route::post("/person/find", "App\Http\Controllers\PersonController@search");
 Route::get("/person/add", "App\Http\Controllers\PersonController@add");
 Route::post("/person/add", "App\Http\Controllers\PersonController@create");
+Route::get("/person/edit", "App\Http\Controllers\PersonController@edit");
+Route::post("/person/edit", "App\Http\Controllers\PersonController@update");
