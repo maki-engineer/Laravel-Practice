61b314804713a845e0207b48b2710ae35734a6fe
maki-engineer <ryohei-81@outlook.jp>
2022-08-19 08:13:16 +0900

    update: Personモデルの修正

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,32 +9,17 @@ use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    use HasFactory;
+   protected $guarded = array('id');
 
-    public function getData()
-    {
-        return $this->id . ": " . $this->name . " (" . $this->age . ")";
-    }
+   public static $rules = array(
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'integer|min:0|max:150'
+   );
 
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where("name", $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where("age", ">=", $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where("age", "<=", $n);
-    }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new ScopePerson);
-    }
+   // getDataは残しておく
+   public function getData()
+   {
+      return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+   }
 }
