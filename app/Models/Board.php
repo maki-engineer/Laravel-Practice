319d3c0d3782a8c332d7578a8dc11e7bcbe5cc9e
maki-engineer <ryohei-81@outlook.jp>
2022-08-19 10:07:33 +0900

    update: Boardモデルを定義

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Board extends Model
 {
     use HasFactory;
+
+    protected $guarded = array("id");
+
+    public static $rules = array(
+        "person_id" => "required",
+        "title"     => "required",
+        "message"   => "required"
+    );
+
+    public function getData()
+    {
+        return $this->id . ": " . $this->title;
+    }
 }
