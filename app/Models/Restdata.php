246eba7d0a752e5b0e25d5a5fe0c20dca88b2612
maki-engineer <ryohei-81@outlook.jp>
2022-08-19 12:43:20 +0900

    update: Restdataモデルを定義

--- app/Models/Restdata.php
+++ app/Models/Restdata.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Restdata extends Model
 {
     use HasFactory;
+
+    protected $table   = "restdata";
+    protected $guarded = array("id");
+
+    public static $rules = array(
+        "message" => "required",
+        "url"     => "required"
+    );
+
+    public function getData()
+    {
+        return $this->id . ":" . $this->message . "(" . $this->url . ")";
+    }
 }
