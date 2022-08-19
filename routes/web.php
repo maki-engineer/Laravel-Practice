85c8d1d9ed42113753cb126c44f678367d02fcbe
maki-engineer <ryohei-81@outlook.jp>
2022-08-19 12:58:38 +0900

    update: index機能とshow機能を実装

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::post("/person/del", "App\Http\Controllers\PersonController@remove");
 Route::get("/board", "App\Http\Controllers\BoardController@index");
 Route::get("/board/add", "App\Http\Controllers\BoardController@add");
 Route::post("/board/add", "App\Http\Controllers\BoardController@create");
+
+Route::resource("/rest", "App\Http\Controllers\RestappController");
