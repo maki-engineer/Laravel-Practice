85921978e237a66cd0d254d5661900a767ab899d
maki-engineer <ryohei-81@outlook.jp>
2022-08-20 11:28:18 +0900

    update: 特定ページの保護

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', "App\Http\Controllers\HelloController@index");
+Route::get('/hello', "App\Http\Controllers\HelloController@index")
+->middleware("auth");
 Route::post("/hello", "App\Http\Controllers\HelloController@post");
 Route::get("/hello/add", "App\Http\Controllers\HelloController@add");
 Route::post("/hello/add", "App\Http\Controllers\HelloController@create");
