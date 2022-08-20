44afaa8fb42c9b79da9f2c768b3781b3b4291c87
maki-engineer <ryohei-81@outlook.jp>
2022-08-20 16:31:00 +0900

    update: テストケース作成

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -2,21 +2,25 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class HelloTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+   public function testHello()
+   {
+       $this->assertTrue(true);
+
+       $arr = [];
+       $this->assertEmpty($arr);
+
+       $msg = "Hello";
+       $this->assertEquals('Hello', $msg);
+
+       $n = random_int(0, 100);
+       $this->assertLessThan(100, $n);
+   }
 }
